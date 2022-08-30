51b17f598aac562868c898fcf18a0e9e7b57a206
iyulkin <iyulkin@gmail.com>
2022-08-30 23:01:37 +0300

    19. Remove Nth Node From End of List

---
+++ src/com/khakimova/medium/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,31 @@
+package com.khakimova.medium;
+
+/**
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ */
+public class RemoveNthNodeFromEndOfList {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode newHead = new ListNode(0, head);
+        ListNode slowPointer = newHead;
+        ListNode fastPointer = newHead;
+
+        while (n > 0) {
+            fastPointer = fastPointer.next;
+            n--;
+        }
+        while (fastPointer.next != null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next;
+        }
+        slowPointer.next = slowPointer.next.next;
+        return newHead.next;
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
