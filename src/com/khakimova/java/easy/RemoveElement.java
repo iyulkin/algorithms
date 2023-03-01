f781318f9139898d027b2a0196a8bda9345d0428
iyulkin <iyulkin@gmail.com>
2023-03-01 21:01:51 +0300

    27. Remove Element

---
+++ src/com/khakimova/java/easy/RemoveElement.java
@@ -0,0 +1,49 @@
+package com.khakimova.java.easy;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+ * <p>
+ * Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+ * <p>
+ * Return k after placing the final result in the first k slots of nums.
+ * <p>
+ * Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+ */
+public class RemoveElement {
+
+    @Test
+    public void test() {
+        int[] nums = new int[]{3, 2, 2, 3};
+        int[] expected = new int[]{2, 2};
+        int k = removeElement(nums, 3);
+        assertEquals(3, k);
+        for (int i = 0; i < k; i++) {
+            assertEquals(expected[i], nums[i]);
+        }
+    }
+
+    private int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                replace(nums, k, i);
+                k++;
+            }
+        }
+        return k;
+    }
+
+    private void replace(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
