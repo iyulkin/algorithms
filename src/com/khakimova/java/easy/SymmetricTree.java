477d659a98dc8e3b128c3ccc30c27236aa4ce445
Asiya Khakimova <asiya.khakimova@simbirsoft.com>
2023-03-29 16:45:09 +0300

    101. Symmetric Tree

---
+++ src/com/khakimova/java/easy/SymmetricTree.java
@@ -0,0 +1,38 @@
+package com.khakimova.java.easy;
+
+import com.khakimova.java.helper.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ */
+public class SymmetricTree {
+
+    @Test
+    public void test() {
+        assertTrue(isSymmetric(new TreeNode(new Integer[]{1, 2, 2, 3, 4, 4, 3})));
+        assertFalse(isSymmetric(new TreeNode(new Integer[]{1, 2, 2, null, 3, null, 3})));
+    }
+
+    private boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return areSymmetric(root.left, root.right);
+    }
+
+    private boolean areSymmetric(TreeNode left, TreeNode right) {
+        if (left == null) {
+            return right == null;
+        }
+        if (right == null) {
+            return left == null;
+        }
+        return left.val == right.val
+                && areSymmetric(left.left, right.right)
+                && areSymmetric(left.right, right.left);
+    }
+}
