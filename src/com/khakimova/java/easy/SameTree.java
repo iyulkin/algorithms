56477b22e8b3f2d517b3d49587e32a4c4e18e2f7
Asiya Khakimova <asiya.khakimova@simbirsoft.com>
2023-03-29 16:48:12 +0300

    100. Same Tree

---
+++ src/com/khakimova/java/easy/SameTree.java
@@ -0,0 +1,28 @@
+package com.khakimova.java.easy;
+
+import com.khakimova.java.helper.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ *
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ */
+public class SameTree {
+
+    @Test
+    public void test() {
+        assertTrue(isSameTree(new TreeNode(new Integer[]{1,2,3}), new TreeNode(new Integer[]{1,2,3})));
+        assertFalse(isSameTree(new TreeNode(new Integer[]{1, 2}), new TreeNode(new Integer[]{1,null,2})));
+        assertFalse(isSameTree(new TreeNode(new Integer[]{1,2,1}), new TreeNode(new Integer[]{1,1,2})));
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        return p == q ||
+                (p != null && q != null && p.val == q.val
+                        && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+    }
+}
