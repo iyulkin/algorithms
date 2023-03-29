a2d571564c25ccc565203510dbb0eeda8ad41252
Asiya Khakimova <asiya.khakimova@simbirsoft.com>
2023-03-29 16:30:20 +0300

    111. Minimum Depth of Binary Tree

---
+++ src/com/khakimova/java/easy/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,38 @@
+package com.khakimova.java.easy;
+
+import com.khakimova.java.helper.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ * <p>
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ * <p>
+ * Note: A leaf is a node with no children.
+ */
+public class MinimumDepthOfBinaryTree {
+
+    @Test
+    public void test() {
+        assertEquals(2, minDepth(new TreeNode(new Integer[]{3, 9, 20, null, null, 15, 7})));
+        assertEquals(5, minDepth(new TreeNode(new Integer[]{2, null, 3, null, 4, null, 5, null, 6})));
+    }
+
+    private int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int lDepth = minDepth(root.left);
+        int rDepth = minDepth(root.right);
+
+        if (lDepth == 0) {
+            return 1 + rDepth;
+        }
+        if (rDepth == 0) {
+            return 1 + lDepth;
+        }
+        return 1 + Math.min(lDepth, rDepth);
+    }
+}
