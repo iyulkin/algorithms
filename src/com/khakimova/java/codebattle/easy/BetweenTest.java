6cc4b78529935535b97181231e4fbc81f0762770
iyulkin <iyulkin@gmail.com>
2023-07-10 09:49:28 +0300

    codebattle: between_text

---
+++ src/com/khakimova/java/codebattle/easy/BetweenTest.java
@@ -0,0 +1,28 @@
+package com.khakimova.java.codebattle.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Write function that returns subtext from text between given patterns.
+ */
+public class BetweenTest {
+    @Test
+    public void test() {
+        Assertions.assertEquals("foo", solution("<a>foo</a>", "<a>", "</a>"));
+        Assertions.assertEquals("text", solution("this is 'text'", "'", "'"));
+    }
+
+    public String solution(String str, String left, String right) {
+        int leftInd = str.indexOf(left);
+        if (leftInd < 0) {
+            return "";
+        }
+        leftInd += left.length();
+        int rightInd = str.lastIndexOf(right);
+        if (rightInd < 0) {
+            return "";
+        }
+        return str.substring(leftInd, rightInd);
+    }
+}
